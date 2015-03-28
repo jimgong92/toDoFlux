c07062ef589b6a1bf90a316cf48a52d4e7ededd7
Jimmy Gong <gong.jim@gmail.com>
2015-03-27 18:46:16 -0700

    Adds TodoItem Component

--- src/js/components/TodoItem.js
+++ src/js/components/TodoItem.js
@@ -1,8 +1,66 @@
 var React = require('react');
+var ReactPropTypes = React.PropTypes;
+var TodoActions = require('../actions/TodoActions');
+var TodoTextInput = require('./TodoTextInput');
+
+var cx = require('react/lib/cx');
 
 var TodoItem = React.createClass({
+  propTypes: {
+    todo: reactPropTypes.object.isRequired
+  },
+  getInitialState: function(){
+    return {
+      isEditing: false
+    };
+  },
+  _onToggleComplete: function(){
+    TodoActions.toggleComplete(this.props.todo);
+  },
+  _onDoubleClick: function(){
+    this.setState({isEditing: true});
+  },
+  _onSave: function(text){
+    TodoActions.updateText(this.props.todo.id, text);
+    this.setState({isEditing: false});
+  },
+  _onDestroyClick: function(){
+    TodoActions.destroy(this.props.todo.id);
+  },
   render: function(){
-    
+    var todo = this.props.todo;
+
+    var input;
+    if (this.state.isEditing){
+      input = 
+        <TodoTextInput
+          className="edit"
+          onSave={this._onSave}
+          value={todo.text} />
+    }
+    return (
+      <li
+        className={cx({
+          'completed': todo.complete,
+          'editing': this.state.isEditing
+        })}
+        key={todo.id}>
+        <div className="view">
+          <input
+            className="toggle"
+            type="checkbox"
+            checked={todo.complete}
+            onChange={this._onToggleComplete}/>
+          <label onDoubleClick={this._onDoubleClick}>
+            {todo.text}
+          </label>
+          <button 
+            className="destroy" 
+            onClick={this._onDestroyClick} />
+        </div>
+        {input}
+      </li>
+    );
   }
 });
 
