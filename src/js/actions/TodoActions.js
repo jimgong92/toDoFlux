ecc6921fb0176e41f761b8827ec15456e0c23850
Jimmy Gong <gong.jim@gmail.com>
2015-03-27 19:00:04 -0700

    Adds Actions

--- src/js/actions/TodoActions.js
+++ src/js/actions/TodoActions.js
@@ -2,5 +2,49 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var TodoConstants = require('../constants/TodoConstants');
 
 var TodoActions = {
-
+  create: function(text){
+    AppDispatcher.dispatch({
+      actionType: TodoConstants.TODO_CREATE,
+      text: text
+    });
+  },
+  updateText : function(id, text){
+    AppDispatcher.dispatch({
+      actionType: TodoConstants.TODO_UPDATE_TEXT,
+      id: id,
+      text: text
+    });
+  },
+  toggleComplete: function(todo){
+    var id = todo.id;
+    if (todo.complete){
+      AppDispatcher.dispatch({
+        actionType: TodoConstants.TODO_UNDO_COMPLETE,
+        id: id
+      });
+    }
+    else {
+      AppDispatcher.dispatch({
+        actionType: TodoConstants.TODO_COMPLETE,
+        id: id
+      });
+    }
+  },
+  toggleCompleteAll: function(){
+    AppDispatcher.dispatch({
+      actionType: TodoConstants.TODO_TOGGLE_COMPLETE_ALL
+    });
+  },
+  destroy: function(id){
+    AppDispatcher.dispatch({
+      actionType: TodoConstants.TODO_DESTROY,
+      id: id
+    });
+  },
+  destroyCompleted: function(){
+    AppDispatcher.dispatch({
+      actionType: TodoConstants.TODO_DESTROY_COMPLETED
+    });
+  }
 };
+
