1c99bf9962930398129c60fda6abf497437dee37
Jimmy Gong <gong.jim@gmail.com>
2015-04-06 15:32:50 -0700

    Allows editing ToDos in database

--- server/controllers/MongoController.js
+++ server/controllers/MongoController.js
@@ -1,39 +1,63 @@
 var ToDo = require('../models/ToDo');
 
+/**
+ * Handles interaction MongoDB
+ */
 module.exports = {
   getAllToDos: function(callback){
-    ToDo.find({}, function(err, todos){
+    ToDo.find({}, function(err, data){
       if (err){
         console.log("Error in Get All ToDos");
         console.log(err);
       }
       if(callback){
-        callback(todos);
+        callback(data);
       }
     });
   },
-  getToDo: function(id){
-    
+  getToDo: function(id, callback){
   },
   addToDo: function(newToDo, callback){
-    ToDo.create(newToDo, function(err, todo){
+    ToDo.create(newToDo, function(err, data){
       if(err){
         console.log("Error in Add New ToDo");
         console.log(err);
       }
-      if(callback) callback(todo);
+      if(callback) callback(data);
     });
   },
-  toggleAllComplete: function(){
-
+  editToDo: function(id, text, callback){
+    ToDo.update({_id: id}, {text: text}, function(err, data){
+      if(err){
+        console.log("Error in Edit ToDo");
+        console.log(err);
+      }
+      console.log("Edited ToDo");
+      console.log(data);
+      if(callback) callback();
+    });
+  },
+  toggleAllComplete: function(callback){
   },
   toggleComplete: function(id){
-
   },
   removeAllComplete: function(){
 
   },
   removeToDo: function(id){
-    
+
   }
 };
+
+/**
+ * Dev Helper Functions
+ * Plug-n-play
+ */
+function clearDB(){
+  ToDo.remove({}, function(err){
+    if (err){
+      console.log("Error clearing database");
+      console.log(err);
+    }
+  });
+}
--- server/router.js
+++ server/router.js
@@ -8,8 +8,8 @@ module.exports = function(app){
     MongoController.getAllToDos(function(todos){
       console.log("Successfully retrieved all ToDos");
       console.log(todos);
+      res.sendStatus(200);
     })
-    res.sendStatus(200);
   });
   /** 
    * Add ToDo
@@ -21,19 +21,44 @@ module.exports = function(app){
     };
     MongoController.addToDo(newToDo, function(){
       console.log("Successfully added ToDo to DB");
+      res.sendStatus(201);
     });
-    res.sendStatus(201);
+  });
+  /**
+   * Edit ToDo
+   */
+  app.post('/api/edit', function(req, res){
+    var id = req.body.id,
+        text = req.body.text;
+    MongoController.editToDo(id, text, function(){
+      console.log("Successfully edited ToDo in DB");
+      res.sendStatus(201);
+    });
   });
   /**
    * Toggle whether ToDo(s) complete
    */
-  app.get('/api/toggle', function(req, res){
-
+  app.post('/api/toggle', function(req, res){
+    if(req.body.id){
+      MongoController.toggle(req.body.id);
+    }
+    else {
+      MongoController.toggleAllComplete();
+    }
+    res.sendStatus(201);
   });
   /**
    * Remove ToDo(s)
    */
   app.post('/api/remove', function(req, res){
-
+    //Remove single ToDo
+    if(req.body.id){
+      MongoController.remove(req.body.id);
+    }
+    //Remove ToDos marked completed
+    else {
+      MongoController.removeAllComplete();
+    }
+    res.sendStatus(201);
   });
 };
