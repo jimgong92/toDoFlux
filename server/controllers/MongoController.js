e368bee7825b2cad25bc1ebd17b5b70d6da34c83
Jimmy Gong <gong.jim@gmail.com>
2015-04-06 03:39:11 -0700

    Adds ToDo post to DB

--- server/controllers/MongoController.js
+++ server/controllers/MongoController.js
@@ -2,10 +2,22 @@ var ToDo = require('../models/ToDo');
 
 module.exports = {
   getAllToDos: function(){
-
+    ToDo.find({}, function(err, todos){
+      console.log("Error in Get All ToDos");
+      console.log(err);
+    });
   },
   getToDo: function(id){
 
+  },
+  addToDo: function(newToDo, callback){
+    ToDo.create(newToDo, function(err, todo){
+      if(err){
+        console.log("Error in Add New ToDo");
+        console.log(err);
+      }
+      if(callback) callback(todo);
+    });
   },
   toggleAllComplete: function(){
 
--- server/router.js
+++ server/router.js
@@ -1,15 +1,25 @@
+var MongoController = require('./controllers/MongoController');
+
 module.exports = function(app){
   /**
    * Get All Todos
    */
   app.get('/api/todos', function(req, res){
+   
     res.sendStatus(200);
   });
   /** 
    * Add ToDo
    */
   app.post('/api/todos', function(req, res){
-
+    var newToDo = {
+      text: req.body.text,
+      isCompleted: false
+    };
+    MongoController.addToDo(req.body, function(){
+      console.log("Successfully added ToDo to DB");
+    });
+    res.sendStatus(201);
   });
   /**
    * Toggle whether ToDo(s) complete
